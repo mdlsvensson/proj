e484e95c345eb494761d51b27960cba0bc84cba0
mdlsvensson <wilmer.lindau@gmail.com>
2025-11-01 13:14:15 +0100

    physics layer applied to tileset

--- scenes/characters/player_character.gd
+++ scenes/characters/player_character.gd
@@ -1,20 +1,14 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-
+const SPEED = 100
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = velocity.lerp(Vector2.ZERO, delta * 15.0) # The '15.0' controls the deceleration rate.
 
 	move_and_slide()
